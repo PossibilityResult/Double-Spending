c70f6d3ccfbb9a74ce8d654ab9644bc1e8e8e179
Elijah <elijahf@umich.edu>
2021-12-12 20:52:37 -0500

    finished ev vis

--- double_spend.py
+++ double_spend.py
@@ -15,14 +15,18 @@ total_defenders = total_validators - total_attackers
 attacker_ratio = total_attackers / total_validators
 defender_ratio = 1 - attacker_ratio
 
-num_samples = 1000
+num_samples = 1001
 largest_attack_payoff = 10000000
 
 attack_payoffs = np.linspace(0, largest_attack_payoff, num_samples)
 
+#source https://decrypt.co/57740/bitcoin-miners-now-earn-1-btc-in-fees-per-block
+transaction_fee = 48000
+
+
 block_reward_btc = 6.25
 bitcoin_price_usd = 50000
-block_reward_usd = block_reward_btc * bitcoin_price_usd
+block_reward_usd = block_reward_btc * bitcoin_price_usd + transaction_fee
 
 ratios = [.05, .1, .25, .5]
 
@@ -33,6 +37,8 @@ for i in range(len(ratios)):
     ev_attack_block_reward_arr = np.full(num_samples, ev_non_attack_br)
     ax.plot(attack_payoffs, ev_attack_block_reward_arr, label = "EV Block Reward")
     ax.legend(prop={'size': 5})
+    ax.set_xticklabels(attack_payoffs, fontsize=5)
+    ax.set_yticklabels(attack_payoffs, fontsize=5)
     ax.set_xlabel("Attacking Payoff", font = {'size' : 7})
     ax.set_ylabel("Expected Value", font = {'size' : 7})
     ax.set_title("Expected Value: k/N = {}".format(ratios[i]), font = {'size' : 10})
