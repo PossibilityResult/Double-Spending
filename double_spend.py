fixing calculation

double_spend.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def ev_attack_success(attacker_ratio, attack_payoff):
-    return (attacker_ratio**2) * attack_payoff
+def ev_attack_success(attacker_ratio, attack_payoff, block_reward):
+    return (attacker_ratio**2) * (attack_payoff + 2 * block_reward)
 
 def ev_attack_block_reward(attacker_ratio, block_reward):
     return attacker_ratio * block_reward
@@ -32,16 +32,16 @@ ratios = [.05, .1, .25, .5]
 
 for i in range(len(ratios)):
     ax = plt.subplot(2, 2, i + 1)
-    ax.plot(attack_payoffs, ev_attack_success(ratios[i], attack_payoffs), label = "EV Attack Reward")
     ev_non_attack_br = ev_attack_block_reward(ratios[i], block_reward_usd)
     ev_attack_block_reward_arr = np.full(num_samples, ev_non_attack_br)
     ax.plot(attack_payoffs, ev_attack_block_reward_arr, label = "EV Block Reward")
+    ax.plot(attack_payoffs, ev_attack_success(ratios[i], attack_payoffs, ev_non_attack_br), label = "EV Attack Reward")
     ax.legend(prop={'size': 5})
     ax.set_xticklabels(attack_payoffs, fontsize=5)
     ax.set_yticklabels(attack_payoffs, fontsize=5)
     ax.set_xlabel("Attacking Payoff", font = {'size' : 7})
     ax.set_ylabel("Expected Value", font = {'size' : 7})
-    ax.set_title("Expected Value: k/N = {}".format(ratios[i]), font = {'size' : 10})
+    ax.set_title("Attacking Computation: k/N = {}".format(ratios[i]), font = {'size' : 10})
 
 plt.tight_layout()
 plt.savefig('EV_attack.png')